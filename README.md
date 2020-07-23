87dfe0ed7cc2b0eded0da85282893426359830db
wh200720041 <wh200720041@vip.qq.com>
2020-07-23 21:27:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# FLOAM 
+## Fast LOAM (Lidar Odometry And Mapping) for solid state lidar (Intel Realsense L515 as an example)
+
+This code is modified from [FLOAM](https://github.com/wh200720041/floam) 
+
+**Modifier:** [Wang Han](http://wanghan.pro), Nanyang Technological University, Singapore
+
+This project is under development, and currently only L515 was tested. Because for solid-state lidar, different company may have different configuration. If you would like to try other senseors, 
+you may test with current version, or email me your sensor brand, configuration as well as the rosbag recording. I will have a quick evaluation when free. 
+
+This project is still under construction because I don't have time to test on robot pltform to further evaluate the performance. You are welcomed to leave feedback of test result on your own platform. Thanks in advance.
+
+## 1. Solid-State Lidar Sensor Example
+<p align='center'>
+<img width="65%" src="/img/realsense_L1515.jpg"/>
+</p>
+Sensor Image
+
+<p align='center'>
+<img width="65%" src="/img/realsense_L1515_description.jpg"/>
+</p>
+
+## 2. Evaluation
+TODO 
+
+
+## 3. Prerequisites
+### 3.1 **Ubuntu** and **ROS**
+Ubuntu 64-bit 18.04.
+
+ROS Melodic. [ROS Installation](http://wiki.ros.org/ROS/Installation)
+
+### 3.2. **Ceres Solver**
+Follow [Ceres Installation](http://ceres-solver.org/installation.html).
+
+### 3.3. **PCL**
+Follow [PCL Installation](http://www.pointclouds.org/downloads/linux.html).
+
+### 3.4. **Trajectory visualization**
+For visualization purpose, this package uses hector trajectory sever, you may install the package by 
+```
+sudo apt-get install ros-melodic-hector-trajectory-server
+```
+Alternatively, you may remove the hector trajectory server node if trajectory visualization is not needed
+
+## 4. Build 
+### 4.1 Clone repository:
+```
+    cd ~/catkin_ws/src
+    git clone https://github.com/wh200720041/floam_ssl.git
+    cd ..
+    catkin_make
+    source ~/catkin_ws/devel/setup.bash
+```
+### 4.2 Dataset
+Currently there are no public dataset for use and I don't have solid-state lidar on hand. 
+You need to use your own sensor or record your own rosbag.
+
+### 4.3 Launch ROS
+```
+    roslaunch floam_ssl floam_ssl.launch
+```
+if you would like to create the map at the same time, you can run (more cpu cost)
+```
+    roslaunch floam floam_mapping.launch
+```
+If the mapping process is slow, you may wish to change the rosbag speed by replacing "--clock -r 0.5" with "--clock -r 0.2" in your launch file, or you can change the map publish frequency manually (default is 10 Hz)
---
+++ src/laserProcessingClass.cpp
@@ -0,0 +1,200 @@
+// Author of FLOAM: Wang Han 
+// Email wh200720041@gmail.com
+// Homepage https://wanghan.pro
+#include "laserProcessingClass.h"
+
+void LaserProcessingClass::init(lidar::Lidar lidar_param_in){
+    
+    lidar_param = lidar_param_in;
+
+}
+
+void LaserProcessingClass::featureExtraction(const pcl::PointCloud<pcl::PointXYZI>::Ptr& pc_in, pcl::PointCloud<pcl::PointXYZI>::Ptr& pc_out_edge, pcl::PointCloud<pcl::PointXYZI>::Ptr& pc_out_surf){
+
+    std::vector<int> indices;
+    //pcl::removeNaNFromPointCloud(*pc_in, indices);
+
+    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> laserCloudScans;
+
+    double last_angle = atan2(pc_in->points[0].z,pc_in->points[0].y) * 180 / M_PI;
+    int count =0;
+    int point_size = pc_in->points.size()-1;
+    for (int i = 0; i < (int) pc_in->points.size(); i++)
+    {
+     
+        //pc_in->points[i].intensity = (double)i / pc_in->points.size();
+        int scanID=0;
+        double distance = sqrt(pc_in->points[i].x * pc_in->points[i].x + pc_in->points[i].y * pc_in->points[i].y + pc_in->points[i].z * pc_in->points[i].z);
+        // if(distance<lidar_param.min_distance || distance>lidar_param.max_distance)
+        //     continue;
+        double angle = atan2(pc_in->points[i].z,pc_in->points[i].y) * 180 / M_PI;
+        //here 5 degree is used for noise tolerance, for L515, the noise is 2 degree
+        // if(i>5000&&i<5300)
+        //     ROS_INFO("%f,%f,%f,%f,%f",angle,pc_in->points[i].x,pc_in->points[i].y,pc_in->points[i].z,pc_in->points[i].intensity);
+        ROS_INFO_ONCE("%f,%f,%f,%f,%f",angle,pc_in->points[0].x,pc_in->points[0].y,pc_in->points[0].z,pc_in->points[0].intensity);
+        ROS_INFO_ONCE("%f,%f,%f,%f,%f",atan2(pc_in->points[point_size].z,pc_in->points[point_size].x) * 180 / M_PI,pc_in->points[point_size].x,pc_in->points[point_size].y,pc_in->points[point_size].z,pc_in->points[point_size].intensity);
+        count++;
+
+        if(fabs(angle - last_angle)>0.05){
+            
+            if(count>100){
+                pcl::PointCloud<pcl::PointXYZI>::Ptr pc_temp(new pcl::PointCloud<pcl::PointXYZI>());
+                for(int k=0;k<count;k++){
+                    pc_temp->push_back(pc_in->points[i-count+k+1]);
+                    pc_in->points[i-count+k+1].intensity=1.0;
+                }
+                laserCloudScans.push_back(pc_temp);
+            }
+            count =0;
+            last_angle = angle;
+        }
+
+    }
+
+    ROS_WARN_ONCE("total points array %d", laserCloudScans.size());
+
+    for(int i = 0; i < laserCloudScans.size(); i++){
+        
+        std::vector<Double2d> cloudCurvature; 
+        int total_points = laserCloudScans[i]->points.size()-10;
+        for(int j = 5; j < (int)laserCloudScans[i]->points.size() - 5; j++){
+            double diffX = laserCloudScans[i]->points[j - 5].x + laserCloudScans[i]->points[j - 4].x + laserCloudScans[i]->points[j - 3].x + laserCloudScans[i]->points[j - 2].x + laserCloudScans[i]->points[j - 1].x - 10 * laserCloudScans[i]->points[j].x + laserCloudScans[i]->points[j + 1].x + laserCloudScans[i]->points[j + 2].x + laserCloudScans[i]->points[j + 3].x + laserCloudScans[i]->points[j + 4].x + laserCloudScans[i]->points[j + 5].x;
+            double diffY = laserCloudScans[i]->points[j - 5].y + laserCloudScans[i]->points[j - 4].y + laserCloudScans[i]->points[j - 3].y + laserCloudScans[i]->points[j - 2].y + laserCloudScans[i]->points[j - 1].y - 10 * laserCloudScans[i]->points[j].y + laserCloudScans[i]->points[j + 1].y + laserCloudScans[i]->points[j + 2].y + laserCloudScans[i]->points[j + 3].y + laserCloudScans[i]->points[j + 4].y + laserCloudScans[i]->points[j + 5].y;
+            double diffZ = laserCloudScans[i]->points[j - 5].z + laserCloudScans[i]->points[j - 4].z + laserCloudScans[i]->points[j - 3].z + laserCloudScans[i]->points[j - 2].z + laserCloudScans[i]->points[j - 1].z - 10 * laserCloudScans[i]->points[j].z + laserCloudScans[i]->points[j + 1].z + laserCloudScans[i]->points[j + 2].z + laserCloudScans[i]->points[j + 3].z + laserCloudScans[i]->points[j + 4].z + laserCloudScans[i]->points[j + 5].z;
+            Double2d distance(j,diffX * diffX + diffY * diffY + diffZ * diffZ);
+            cloudCurvature.push_back(distance);
+
+        }
+
+        featureExtractionFromSector(laserCloudScans[i],cloudCurvature, pc_out_edge, pc_out_surf);
+            
+    
+    }
+
+}
+
+
+void LaserProcessingClass::featureExtractionFromSector(const pcl::PointCloud<pcl::PointXYZI>::Ptr& pc_in, std::vector<Double2d>& cloudCurvature, pcl::PointCloud<pcl::PointXYZI>::Ptr& pc_out_edge, pcl::PointCloud<pcl::PointXYZI>::Ptr& pc_out_surf){
+
+    std::sort(cloudCurvature.begin(), cloudCurvature.end(), [](const Double2d & a, const Double2d & b)
+    { 
+        return a.value < b.value; 
+    });
+
+
+    int largestPickedNum = 0;
+    std::vector<int> picked_points;
+    int point_info_count =0;
+    for (int i = cloudCurvature.size()-1; i >= 0; i--)
+    {
+        int ind = cloudCurvature[i].id; 
+        if(std::find(picked_points.begin(), picked_points.end(), ind)==picked_points.end()){
+            if(cloudCurvature[i].value <= 0.1){
+                break;
+            }
+            
+            largestPickedNum++;
+            picked_points.push_back(ind);
+            
+            if (largestPickedNum <= 10){
+                pc_out_edge->push_back(pc_in->points[ind]);
+                point_info_count++;
+            }else{
+                break;
+            }
+
+            for(int k=1;k<=5;k++){
+                double diffX = pc_in->points[ind + k].x - pc_in->points[ind + k - 1].x;
+                double diffY = pc_in->points[ind + k].y - pc_in->points[ind + k - 1].y;
+                double diffZ = pc_in->points[ind + k].z - pc_in->points[ind + k - 1].z;
+                if (diffX * diffX + diffY * diffY + diffZ * diffZ > 0.05){
+                    break;
+                }
+                picked_points.push_back(ind+k);
+            }
+            for(int k=-1;k>=-5;k--){
+                double diffX = pc_in->points[ind + k].x - pc_in->points[ind + k + 1].x;
+                double diffY = pc_in->points[ind + k].y - pc_in->points[ind + k + 1].y;
+                double diffZ = pc_in->points[ind + k].z - pc_in->points[ind + k + 1].z;
+                if (diffX * diffX + diffY * diffY + diffZ * diffZ > 0.05){
+                    break;
+                }
+                picked_points.push_back(ind+k);
+            }
+
+        }
+    }
+
+    //find flat points
+    // point_info_count =0;
+    // int smallestPickedNum = 0;
+    
+    // for (int i = 0; i <= (int)cloudCurvature.size()-1; i++)
+    // {
+    //     int ind = cloudCurvature[i].id; 
+
+    //     if( std::find(picked_points.begin(), picked_points.end(), ind)==picked_points.end()){
+    //         if(cloudCurvature[i].value > 0.1){
+    //             //ROS_WARN("extracted feature not qualified, please check lidar");
+    //             break;
+    //         }
+    //         smallestPickedNum++;
+    //         picked_points.push_back(ind);
+            
+    //         if(smallestPickedNum <= 4){
+    //             //find all points
+    //             pc_surf_flat->push_back(pc_in->points[ind]);
+    //             pc_surf_lessFlat->push_back(pc_in->points[ind]);
+    //             point_info_count++;
+    //         }
+    //         else{
+    //             break;
+    //         }
+
+    //         for(int k=1;k<=5;k++){
+    //             double diffX = pc_in->points[ind + k].x - pc_in->points[ind + k - 1].x;
+    //             double diffY = pc_in->points[ind + k].y - pc_in->points[ind + k - 1].y;
+    //             double diffZ = pc_in->points[ind + k].z - pc_in->points[ind + k - 1].z;
+    //             if (diffX * diffX + diffY * diffY + diffZ * diffZ > 0.05){
+    //                 break;
+    //             }
+    //             picked_points.push_back(ind+k);
+    //         }
+    //         for(int k=-1;k>=-5;k--){
+    //             double diffX = pc_in->points[ind + k].x - pc_in->points[ind + k + 1].x;
+    //             double diffY = pc_in->points[ind + k].y - pc_in->points[ind + k + 1].y;
+    //             double diffZ = pc_in->points[ind + k].z - pc_in->points[ind + k + 1].z;
+    //             if (diffX * diffX + diffY * diffY + diffZ * diffZ > 0.05){
+    //                 break;
+    //             }
+    //             picked_points.push_back(ind+k);
+    //         }
+
+    //     }
+    // }
+    
+    for (int i = 0; i <= (int)cloudCurvature.size()-1; i++)
+    {
+        int ind = cloudCurvature[i].id; 
+        if( std::find(picked_points.begin(), picked_points.end(), ind)==picked_points.end())
+        {
+            pc_out_surf->push_back(pc_in->points[ind]);
+        }
+    }
+    
+
+
+}
+LaserProcessingClass::LaserProcessingClass(){
+    
+}
+
+Double2d::Double2d(int id_in, double value_in){
+    id = id_in;
+    value =value_in;
+};
+
+PointsInfo::PointsInfo(int layer_in, double time_in){
+    layer = layer_in;
+    time = time_in;
+};
